69c3d3d42ebf96e9314640e87a77ccec0777bb1e
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2021-10-29 21:43:35 -0300

    add CardList array

---
+++ src/utils/CardList.ts
@@ -0,0 +1,46 @@
+import Bulbasaur from '../images/Bulbasaur.png';
+import Butterfree from '../images/Butterfree.png';
+import Caterpie from '../images/Caterpie.png';
+import Charmander from '../images/Charmander.png';
+import Pidgey from '../images/Pidgey.png';
+import Rattata from '../images/Rattata.png';
+import Squirtle from '../images/Squirtle.png';
+import Weedle from '../images/Weedle.png';
+import { Card } from '../types';
+
+const cards: Card[] = [
+    {
+        name: 'Bulbasaur',
+        src: Bulbasaur,
+    },
+    {
+        name: 'Butterfree',
+        src: Butterfree,
+    },
+    {
+        name: 'Caterpie',
+        src: Caterpie,
+    },
+    {
+        name: 'Charmander',
+        src: Charmander,
+    },
+    {
+        name: 'Pidgey',
+        src: Pidgey,
+    },
+    {
+        name: 'Rattata',
+        src: Rattata,
+    },
+    {
+        name: 'Squirtle',
+        src: Squirtle,
+    },
+    {
+        name: 'Weedle',
+        src: Weedle,
+    }
+];
+
+export default cards;
