31119541c0e04e3054c0f36bae82c12f62281c44
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2021-11-04 12:15:42 -0300

    Create hook to fetch images and configure queryClient object

---
+++ src/hooks/useRequests.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+import { Card } from "../types";
+
+const endpointsPokemon = () => {
+    let result = [];
+    for (let i = 1; i <= 151; i++) {
+        result.push(axios(`https://pokeapi.co/api/v2/pokemon/${i}/`));
+    }
+    return result;
+}
+
+const endpointsRM = () => {
+    let result = [];
+    for (let i = 1; i <= 5; i++) {
+        result.push(axios(`https://rickandmortyapi.com/api/character?page=${i}`));
+    }
+    return result;
+}
+
+const useRequests = () => {
+    const pokemonQuery = useQuery<Card[] | undefined>('pokemon', async () => {
+        let result = await Promise.all(endpointsPokemon());
+        return result.map(({ data }) => ({
+            id: `${data.id}`,
+            name: data.name,
+            src: data.sprites.other.dream_world.front_default
+        }));
+    });
+
+    const rickAndMortyQuery = useQuery<Card[] | undefined>('rickAndMorty', async () => {
+        let resultPromise = await Promise.all(endpointsRM());
+        let result: Array<{ id: number, name: string, image: string }> = [];
+        resultPromise.forEach((res) => {
+            result.push(...res.data.results);
+        });
+
+        return result.map(res => ({
+            id: `${res.id}`,
+            name: res.name,
+            src: res.image
+        }));
+    });
+
+    return { pokemonQuery, rickAndMortyQuery };
+}
+
+export default useRequests;
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { GameProvider } from './Context';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0, // Cantidad de intentos que queremos que la libreria realice antes de lanzar un error en caso de que la request falle.
+      staleTime: Infinity, // Cantidad de tiempo para que la libreria considere "stale" la query.
+      // notifyOnChangePropsExclusions: ['isStale'], // Notifica que la data cacheada esta "stale", esto produce un re-render.
+      // refetchOnWindowFocus: false, // Para desactivar el refetch cuando cambiamos de ventana.
+    }
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GameProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </GameProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
