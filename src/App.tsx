c440140abdeb7aed1c59ac58c34020c7a6b8bb58
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2021-11-04 12:17:36 -0300

    Add hooks and modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import './App.css';
-import Board from './components/Board';
+import { Suspense, lazy } from 'react';
+import SelectionModal from './components/SelectionModal';
+import useGameContext from './hooks/useGameContext';
+import useRequests from './hooks/useRequests';
+
+const Board = lazy(() => import(/* webpackPrefetch: true */ './components/Board'));
 
 function App() {
+
+  const { game } = useGameContext();
+  useRequests();
+
+  console.log('render');
+
   return (
     <div className="App">
-      <Board />
+      {
+        !game
+          ?
+          <SelectionModal />
+          :
+          <Suspense fallback={<div>cargando.......</div>}>
+            <Board />
+          </Suspense>
+      }
     </div>
   );
 }
