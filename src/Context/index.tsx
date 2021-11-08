86015f71f1bab2aeed5a1000e339998a48c0f7e0
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2021-11-07 22:05:54 -0300

    Create new context called ContextSound

--- src/Context/index.tsx
+++ src/Context/index.tsx
@@ -1,15 +1,30 @@
 import { createContext, FC, useState } from 'react';
 
-interface Value {
+interface Game {
     game: string
     setGame: React.Dispatch<React.SetStateAction<string>>
 }
 
-export const Context = createContext<Value | null>(null);
+interface Sound {
+    soundActive: boolean
+    setSoundActive: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const ContextGame = createContext<Game | null>(null);
+
+export const ContextSound = createContext<Sound | null>(null);
 
 export const GameProvider: FC = ({ children }) => {
     const [game, setGame] = useState('');
 
-    return <Context.Provider value={{ game, setGame }}>{children}</Context.Provider>
+    return <ContextGame.Provider value={{ game, setGame }}>{children}</ContextGame.Provider>
 }
+
+export const SoundProvider: FC = ({ children }) => {
+    const [soundActive, setSoundActive] = useState(true);
+
+    return <ContextSound.Provider value={{ soundActive, setSoundActive }}>{children}</ContextSound.Provider>
+}
+
+
 
