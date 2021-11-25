a182eaf592b59eb7cb70b7f23b8a29be2e811eaf
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2021-11-25 00:03:46 -0300

    Add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react"
+import { QueryClient, QueryClientProvider } from "react-query";
+import App from './App'
+import { GameProvider, SoundProvider } from "./Context";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 0, // Cantidad de intentos que queremos que la libreria realice antes de lanzar un error en caso de que la request falle.
+            staleTime: Infinity, // Cantidad de tiempo para que la libreria considere "stale" la query.
+            // notifyOnChangePropsExclusions: ['isStale'], // Notifica que la data cacheada esta "stale", esto produce un re-render.
+            // refetchOnWindowFocus: false, // Para desactivar el refetch cuando cambiamos de ventana.
+        }
+    }
+});
+
+test('should render App', () => {
+    const { getByRole, queryByRole, getAllByRole, getByAltText } = render(
+        <SoundProvider>
+            <GameProvider>
+                <QueryClientProvider client={queryClient}>
+                    <App />
+                </QueryClientProvider>
+            </GameProvider>
+        </SoundProvider>
+    );
+    const soundButtonOff = getByRole('button', { name: 'turn sound off' });
+    const soundButtonOn = queryByRole('button', { name: 'turn sound on' });
+
+    //clickear boton de sonido y testear que este el otro
+
+    const images = getAllByRole('img');
+    // que esten las imagenes, verificar que esten los iconos en footer (poner aria laberl (visitar mi...) en cada link y agarrarlos por aria laber)
+
+
+    expect(soundButtonOff).toBeVisible();
+    expect(soundButtonOn).not.toBeInTheDocument();
+    expect(images).toHaveLength(3);
+
+
+
+
 });
