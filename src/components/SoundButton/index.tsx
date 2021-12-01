7393a31a6062bb7233e07f694c9fd4d9290a8f84
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2021-12-01 16:51:17 -0300

    Add sounds to soundButton

--- src/components/SoundButton/index.tsx
+++ src/components/SoundButton/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
 import useSoundContext from '../../hooks/useSoundContext';
 import { StyledSoundButton } from './StyledSoundButton';
+import useSound from 'use-sound';
+import offSound from '../../assets/sounds/offSound.mp3';
+import onSound from '../../assets/sounds/onSound.mp3';
+
 
 const SoundButton = () => {
     const { soundActive, setSoundActive } = useSoundContext();
+    const [playOff] = useSound(offSound, { volume: 0.50 });
+    const [playOn] = useSound(onSound, { volume: 0.25 });
+
+    const handleClick = () => {
+        setSoundActive(prev => !prev);
+        if (soundActive) {
+            playOff();
+        } else {
+            playOn();
+        }
+    }
 
     return (
-        <StyledSoundButton onClick={() => setSoundActive(prev => !prev)} aria-label={soundActive ? 'turn sound off' : 'turn sound on'}>
+        <StyledSoundButton onClick={handleClick} aria-label={soundActive ? 'turn sound off' : 'turn sound on'}>
             {
                 soundActive
                     ?
